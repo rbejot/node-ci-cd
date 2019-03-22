c02cb56aa3b83423f70999e60e7eb396c89670b1
Rodolphe <rbejot@student.42.fr>
2019-03-22 17:32:44 +0100

    add deploy script

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.get('/', (req, res) => {
-  res.send('/');
+  res.send('hey');
 });
 
 app.listen(process.env.PORT || 3000);
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('GET /', () => {
-  it('respond with /', done => {
-    request(app).get('/').expect('/', done);
+  it('respond with hey', done => {
+    request(app).get('/').expect('hey', done);
   });
 });
